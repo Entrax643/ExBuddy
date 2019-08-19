d2d8a8f00689d73a4ff863200c2604bef1c4549a
Insanepoet <rfinchamsonata@gmail.com>
2019-08-19 08:09:08 -0400

    Update UnBlockAll.ps1
    Increased  path check length as I though of a way someone could screw up extraction.
    
    Added additional comments for readability

--- ExBuddy/UnBlockAll.ps1
+++ ExBuddy/UnBlockAll.ps1
@@ -2,10 +2,12 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
 # Test Pathing
-if (Test-Path ..\..\Plugins\ExBuddy) {
+if (Test-Path ..\..\Plugins\ExBuddy\Agents) {
+  # Path Correct Unblock all and confirm success to user
   Get-ChildItem -Path '..\ExBuddy' -Recurse | Unblock-File
   [System.Windows.Forms.MessageBox]::Show("Exbuddy was correctly installed and is now UnBlocked","Success",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Asterisk)
 }
 else {
+  # Path Incorrect Warn User and exit
   [System.Windows.Forms.MessageBox]::Show("It seems as though Exbuddy has not been extracted into the correct folder, path should be <rbfolder>\Plugins\Exbuddy","Error",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Error)
 }
