28adbd7ca469cd795e12f1ab2f35aa37ec0fb6a3
Insanepoet <rfinchamsonata@gmail.com>
2019-08-19 07:54:42 -0400

    Added Shortcut and PS1 file for automatic file unblocking
    As ExBuddy is recommended to be installed for new users via zip download, files will need to be unblocked once extracted. I have added a powershell script and a shortcut to open powershell and run a script checking the install location and if correct unblocking all files recursively within the Exbuddy folder.

---
+++ ExBuddy/UnBlockAll.ps1
@@ -0,0 +1,11 @@
+# Load assembly
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+# Test Pathing
+if (Test-Path ..\..\Plugins\ExBuddy) {
+  Get-ChildItem -Path '..\Plugins\ExBuddy' -Recurse | Unblock-File
+  [System.Windows.Forms.MessageBox]::Show("Exbuddy was correctly installed and is now UnBlocked","Success",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Asterisk)
+}
+else {
+  [System.Windows.Forms.MessageBox]::Show("It seems as though Exbuddy has not been extracted into the correct folder, path should be <rbfolder>\Plugins\Exbuddy","Error",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Error)
+}
